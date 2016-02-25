66c562591da5ac8c44d9836dea27e55d3125eee5
Elizabeth Evans <ickabeth@gmail.com>
2016-02-25 14:00:20 -0500

    added more js

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-// var messages = {
-//   username: andrew,
-//
-// }
 
+var templates = {
+  message: [
+    "<div data-postid='<%= _id %>'>",
+       "<p> <%=userName%> <%= content %> </p>",
+      "<input type='text' name='editContent' value='<%= content %>'>",
+       "<button class='submitEdit'>Submit Edit</button>",
+       "</div>"
+  ].join("")
+}
 
-var url = "https://tiny-tiny.herokuapp.com/collections/faim";
-
-
-$.ajax({
-  method: "post",
-  url: url,
-  data: {
-    username: "andrew",
-    password: "123",
+var faim = {
+  url: 'http://tiny-tiny.herokuapp.com/collections/faim',
+  init: function () {
+    faim.initEvents();
+    faim.initStyling();
   },
-  success: function(data){
-    console.log(data);
-  },
-  error: function (error) {
-    console.log(error);
+
+  initEvents: function () {
+  $('form').on('submit', faim.submitPost);
+  $('section').on('click', '.delete', faim.deletePostFromDom);
+},
+
+submitMessage: function (event) {
+  event.preventDefault();
+  var   newMessage = faim.getMessageFromDom();
+  console.log(newMessage);
+    faim.addMessage(newMessage);
+
+    $('input').val('');
+},
+getMessageFromDom: function getMessageFromDom() {
+  var content = $('input[name="content"]').val();
+  return {
+    content: content
   }
-});
-
-$.ajax({
-  method: "post",
-  url: url,
-  data: {
-    username: "elizabeth",
-    password: "321"
-  },
-  success: function (data) {
-    console.log(data);
-  },
-  error: function (error) {
-    console.log(error);
-  }
-})
+},
+addAllMessagesToDom: function(event){
+  $('.messageArea').html('');
+  _.each(event, function(element){
+    var tmpl = _.template(templates.message);
+    $(".messageArea").append(tmpl(element));
+  })
+},
+getMessages: function getMessages(){
+  return faim;
+}
+}
